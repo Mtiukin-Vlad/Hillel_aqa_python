6b503ca7258414b21707ee31525572bddea88a37
Vladyslav Matiukhin <vladyslav.matiukhin@gmail.com>
2025-04-23 17:42:16 +0200

    Add fixes for homework №10

--- lesson_10/test_cases.py
+++ lesson_10/test_cases.py
@@ -4,31 +4,29 @@ from homework_10 import log_event
 
 LOG_FILE = 'login_system.log'
 
-class TestPositiveUserLogin:
+class TestUserLogin:
 
     def test_success_log(self):
         log_event("Vlad", "success")
         with open(LOG_FILE, "r") as file:
             content = file.read()
-        assert "Username: Vlad, Status: success" in content
+        assert "Username: Vlad" in content
+        assert "Status: success" in content
+        assert "INFO" in content
 
-    def test_success_log_full_name(self):
-        log_event("Vlad Matiukhin", "success")
-        with open(LOG_FILE, "r") as file:
-            content = file.read()
-        assert "Username: Vlad Matiukhin, Status: success" in content
-
-
-class TestNegativeUserLogin:
 
     def test_expired_log(self):
         log_event("Vlad", "expired")
         with open(LOG_FILE, "r") as file:
             content = file.read()
-        assert "Username: Vlad, Status: expired" in content
+        assert "Username: Vlad" in content
+        assert "Status: expired" in content
+        assert "WARNING" in content
 
     def test_empty_username_failed(self):
         log_event("", "failed")
         with open(LOG_FILE, "r") as file:
             content = file.read()
-        assert "Username: , Status: failed" in content
+        assert "Username: " in content
+        assert "Status: failed" in content
+        assert "ERROR" in content
