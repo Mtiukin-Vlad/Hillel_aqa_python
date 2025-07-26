2e989a6ac33a9dc622e9c3926e48c4d0618ea726
Vladyslav Matiukhin <vladyslav.matiukhin@gmail.com>
2025-07-26 23:20:55 +0200

    add hw fixes

--- lesson_24/test_frames_verification.py
+++ lesson_24/test_frames_verification.py
@@ -1,22 +1,18 @@
-from selenium import webdriver
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import time
 
-def test_frame_verification():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def test_frame_verification(driver):
+    # Відкриваю головну сторінку з фреймами
     driver.get("http://localhost:8000/dz.html")
 
     # Перехід у перший фрейм
     driver.switch_to.frame("frame1")
-    input1 = driver.find_element(By.ID, "input1")
-    input1.send_keys("Frame1_Secret")
+    driver.find_element(By.ID, "input1").send_keys("Frame1_Secret")
     driver.find_element(By.TAG_NAME, "button").click()
 
+    time.sleep(1)  # Даємо час алерту з’явитись
     alert = Alert(driver)
-    time.sleep(1)
     assert alert.text == "Верифікація пройшла успішно!"
     alert.accept()
 
@@ -25,13 +21,10 @@ def test_frame_verification():
 
     # Перехід у другий фрейм
     driver.switch_to.frame("frame2")
-    input2 = driver.find_element(By.ID, "input2")
-    input2.send_keys("Frame2_Secret")
+    driver.find_element(By.ID, "input2").send_keys("Frame2_Secret")
     driver.find_element(By.TAG_NAME, "button").click()
 
-    alert = Alert(driver)
     time.sleep(1)
+    alert = Alert(driver)
     assert alert.text == "Верифікація пройшла успішно!"
     alert.accept()
-
-    driver.quit()
